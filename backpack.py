05ac0351b3b731cea39c4c23883fb858763aae3c
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-01-22 20:29:31 +0100

    Creating usr profile with for loop

--- backpack.py
+++ backpack.py
@@ -1,1 +1,9 @@
 #Simple two-players game. Player has to guess what is hidden in his mate's backpack.
+
+players = []
+for x in range(2):
+    players.append({
+        'user':'',
+        'points':0,
+        'backpack':[]
+    })
