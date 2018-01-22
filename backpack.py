673ec20e6891650f3fe12d0b59d6c01fa01f659b
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-01-22 21:56:32 +0100

    added solutions for gathering info about usr

--- backpack.py
+++ backpack.py
@@ -7,3 +7,11 @@ for x in range(2):
         'points':0,
         'backpack':[]
     })
+    players[x]['user'] = input('Provide name for player ' + str(x + 1) + ': ')
+    print('Enter 4 things that you are going to hide into backpack:')
+    for y in range(4):
+        backpack_items = input('an item name: ')
+        players[x]['backpack'].append(backpack_items)
+
+print(players)
+
